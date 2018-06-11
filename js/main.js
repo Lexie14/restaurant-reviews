a075e8680707881ae2705a0976d37d6e46270beb
Lexie <oleksandrapas@gmail.com>
2018-06-11 19:08:58 +0200

    alt attribute added to all images: - imageinfo property added in restaurants.json; - alt attribute with the text "imageinfo" added to the images in restaurants.js and main.js.

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'An image of the ' + restaurant.imageinfo;
   li.append(image);
 
   const name = document.createElement('h1');
